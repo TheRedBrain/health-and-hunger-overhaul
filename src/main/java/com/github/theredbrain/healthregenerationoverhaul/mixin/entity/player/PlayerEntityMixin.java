bf25d8d30e4db0529bfa44ab243f691ae8387020
TheRedBrain <thetrueredbrain@gmail.com>
2024-07-25 23:25:20 +0200

    applyDamage now uses heal instead of setHealth

--- src/main/java/com/github/theredbrain/healthregenerationoverhaul/mixin/entity/player/PlayerEntityMixin.java
+++ src/main/java/com/github/theredbrain/healthregenerationoverhaul/mixin/entity/player/PlayerEntityMixin.java
@@ -1,18 +1,26 @@
 package com.github.theredbrain.healthregenerationoverhaul.mixin.entity.player;
 
 import com.github.theredbrain.healthregenerationoverhaul.entity.HealthRegeneratingEntity;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity implements HealthRegeneratingEntity {
 
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Redirect(method = "applyDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setHealth(F)V"))
+    protected void healthregenerationoverhaul$redirect_setHealth(PlayerEntity instance, float v, @Local(argsOnly = true) float amount) {
+        instance.heal(-amount);
     }
 
     @Override
